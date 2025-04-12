cc3ef8884d05e0d88032c2eeafd23d39d5a5a72c
DeokJae Kim <kdj0712@gmail.com>
2025-04-12 12:40:07 +0900

    MvcConfig fix

--- src/main/java/com/yojulab/study_springboot/configs/WebMvcConfig.java
+++ src/main/java/com/yojulab/study_springboot/configs/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.yojulab.study_springboot.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 스프링 빈으로 등록
@@ -12,13 +13,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
         registry.addMapping("/**")
-                // Origin이 http:localhost:3000에 대해
-                // .allowedOrigins("http://*:5500", "http://127.0.0.1:5500", "http://192.168.0.30:5500", "http://192.168.0.41:5500", "http://192.168.0.70:5500")
-                .allowedOriginPatterns("*")
-                // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(MAX_AGE_SECS);
+            // Origin이 http:localhost:3000에 대해
+            // .allowedOrigins("http://*:5500", "http://127.0.0.1:5500", "http://192.168.0.30:5500", "http://192.168.0.41:5500", "http://192.168.0.70:5500")
+            .allowedOriginPatterns("*")
+            // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(MAX_AGE_SECS);
+                
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // FastAPI의 이미지 리소스를 프록시로 처리
+        registry.addResourceHandler("/data/img/**")
+               .addResourceLocations("http://rare-field.shop:80/data/img/");
     }
 }
